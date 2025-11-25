6dd2e7f2682014e34e8da886b667a2386427756b
myriamortiz <myriamortiz25@gmail.com>
2025-11-25 22:21:30 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("fbs-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/manifest.json",
+        "/assets/logo/icon-192.png",
+        "/assets/logo/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
