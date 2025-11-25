5c7a6e81f4b409dee27fd8b1bbc46effcc85765f
myriamortiz <myriamortiz25@gmail.com>
2025-11-25 19:52:00 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("FitBloom Studio – app.js chargé ✅");
+
+// Exemple : plus tard, on ira chercher les données JSON ici
+// fetch("./data/meals.json")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log("Meals data:", data);
+//   })
+//   .catch((err) => console.error(err));
