b2e7096f18e7a482e600b92962c075f09447d690
myriamortiz <myriamortiz25@gmail.com>
2025-11-30 03:38:14 +0100

    Update selfcare.js

--- selfcare.js
+++ selfcare.js
@@ -251,25 +251,22 @@ function updateComplements() {
 updateComplements();
 
 // -------------------------------
-// RECAP DE LA SEMAINE
+// Récap de la semaine
 // -------------------------------
 const weeklyRecap = document.getElementById("weekly-recap");
 
 function generateWeeklyRecap() {
-  // Récupérer l'émotion enregistrée
   const emotion = localStorage.getItem("emotion") || "Aucune émotion enregistrée";
-
-  // Récupérer les tâches depuis localStorage
   const todosStatus = JSON.parse(localStorage.getItem("todos")) || [];
   const todosDone = todosStatus.filter(todo => todo.checked).length;
 
-  // Si il n'y a pas d'émotion ou de tâches, afficher le message par défaut
-  if (!emotion && todosStatus.length === 0) {
+  if (currentWeek === 1) {
+    // Si la semaine n'a pas encore commencé, afficher le message par défaut
     weeklyRecap.innerHTML = `
       <p>En attente du début de la semaine...</p>
     `;
   } else {
-    // Mettre à jour le récapitulatif avec les données disponibles
+    // Sinon, afficher les données disponibles
     weeklyRecap.innerHTML = `
       <p>Emotion de la semaine : ${emotion}</p>
       <p>Tâches complètes : ${todosDone} / ${todosStatus.length}</p>
